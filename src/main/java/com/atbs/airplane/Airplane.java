e4f1a57e55d3acdf5b7b5c656a03492bdf7e2dea
Prakash Rai <prai@mum.edu>
2018-01-27 12:07:52 -0500

    airplane

--- src/main/java/com/atbs/airplane/Airplane.java
+++ src/main/java/com/atbs/airplane/Airplane.java
@@ -17,4 +17,20 @@ public class Airplane extends BaseEntity {
     @NotNull
     @Column(name = "capacity")
     private  int  capacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
